2facf7dc3d4d8fe713fdf06e5d10efbcfb6de697
Premor <spamerkik@gmail.com>
2018-04-08 23:50:41 +0300

    add 1 laba

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,33 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    x <- getLine
+    print $ anotherFunc <$> someFunc x
+
+laba1_3 :: Test -> String
+data Test = Gavno Int
+          | Jopa Int
+          | Chlen Int
+          deriving (Eq,Show,Ord)
+
+data Data = Q | W | E | R deriving(Show)
+
+laba1_4 :: Data -> String
+
+laba1_4 Q = "QA"
+
+laba1_4 W = "WA"
+
+laba1_4 E = "EA"
+
+laba1_4 R = "RA"
+
+
+laba1_3 (Gavno 1) = "Gavno"
+
+laba1_3 (Jopa  2) = "Jopa"
+
+laba1_3 (Chlen  3) = "Gavno"
+
+laba1_3 (Gavno x) = "SYKA"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,34 @@
 module Lib
-    ( someFunc
+    ( 
+        someFunc,
+        anotherFunc,
+        laba1_1,
+        laba1_2
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc :: String -> Maybe Int 
+anotherFunc :: Int -> Bool
+laba1_1 :: Int -> Int -> Int
+laba1_2 :: [Int] -> [Int]
+
+
+laba1_1 x y 
+    | y == 0 = 1    
+    | y == 1 = x
+    | otherwise = laba1_1 (x*2) (y-1)
+
+laba1_2 x = [i * i | i <- x]
+
+
+
+
+
+someFunc x =
+        let i =read x::Int in
+        if i > 10 
+            then Just i
+            else Nothing 
+
+anotherFunc x 
+        | x == 0 = True
+        | otherwise = False
